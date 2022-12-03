657ef20b0126c1cf55d5b5e98533982f2c9f9764
Italo Santos <94938791+KayleKylian@users.noreply.github.com>
2022-12-03 16:34:37 +0000

    Início do bot

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using Telegram.Bot;
+
+var botClient = new TelegramBotClient("{$BOTTOKEN}");
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}");
